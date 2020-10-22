3771ca16eb5ffe50b203494f05a0a8b09cc170b8
SongTianhe <49253479+SongTianhe@users.noreply.github.com>
2020-10-22 16:37:58 +0200

    oppretet main class, testet og funker uten syntaksfeil, nå er ferdig med alle innledning, klar til oppgave1

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -131,5 +131,10 @@ public class EksamenSBinTre<T> {
         throw new UnsupportedOperationException("Ikke kodet ennå!");
     }
 
+    public static void main(String[] args) {
+        EksamenSBinTre<String> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
+        System.out.println(tre.antall());
+    }
+
 
 } // ObligSBinTre
